3a9f6a3160c2ffb3de6d164d3d9fa0e140117e73
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2022-03-25 09:06:45 -0400

    Create app.cs

---
+++ app.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int Ran = rnd.Next(1, 100);
+            Random rnd1 = new Random();
+            int Ran1 = rnd1.Next(1, 100);
+            int score = 0; 
+            Console.WriteLine("Let's test your math skills!");
+            Console.WriteLine("You are required to add a random integer to a random number and see how far you get!");
+            Console.WriteLine("The randmon number is " + Ran + ", and the interger you must add by is " + Ran1 + ".");
+            do 
+            {
+            }
+            while
+        }
+    }
+}
