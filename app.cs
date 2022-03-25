abd4d86df20fab064c94ab85645563e5c65bfe8e
Huzayl-Yusufzai <95150897+Huzayl-Yusufzai@users.noreply.github.com>
2022-03-25 09:37:27 -0400

    Update app.cs

--- app.cs
+++ app.cs
@@ -13,7 +13,8 @@ namespace ConsoleApplication1
             int Ran = rnd.Next(1, 100);
             Random rnd1 = new Random();
             int Ran1 = rnd1.Next(1, 100);
-            int score = 0; 
+            int score = 0;
+            int next = 0;
             Console.WriteLine("Let's test your math skills!");
             Console.WriteLine("You are required to add a random integer to a random number and see how far you get!");
             Console.WriteLine("The randmon number is " + Ran + ", and the interger you must add by is " + Ran1 + ".");
@@ -32,8 +33,9 @@ namespace ConsoleApplication1
                }
                do
                {
-               stageAnswer 
+               stageAnswer = stageAnswer + Ran1; 
                }
+               while (1==2);
             }
         }
     }
