7bf36eb7da46639f823198c6de89f41ea8cfcf47
Huzayl-Yusufzai <95150897+Huzayl-Yusufzai@users.noreply.github.com>
2022-03-25 17:41:51 -0400

    Update app.cs

--- app.cs
+++ app.cs
@@ -14,30 +14,31 @@ namespace ConsoleApplication1
             Random rnd1 = new Random();
             int Ran1 = rnd1.Next(1, 100);
             int score = 0;
-            int next = 0;
+            int stageAnswer = Ran + Ran1;
             Console.WriteLine("Let's test your math skills!");
             Console.WriteLine("You are required to add a random integer to a random number and see how far you get!");
             Console.WriteLine("The randmon number is " + Ran + ", and the interger you must add by is " + Ran1 + ".");
-            while (1==1)
+            while (1 == 1)
             {
-               int stageAnswer = Ran + Ran1;
-               int userAnswer = Convert.ToInt32(Console.ReadLine());
-               if ((userAnswer > stageAnswer) || (userAnswer < stageAnswer))
-               {
-                   break; 
-               }
-               else 
-               {
-                   score++;
-                   Console.WriteLine(score);
-               }
-               do
-               {
-               stageAnswer = stageAnswer + Ran1; 
-               }
-               while (1==2);
-               Console.ReadLine(); 
+                int userAnswer = Convert.ToInt32(Console.ReadLine());
+                if (userAnswer == stageAnswer)
+                {
+                    score = score + 1;
+                    Console.WriteLine("Your current score is " + score + ". Press 'Enter' and then type your next answer");
+                    do
+                    {
+                        stageAnswer = stageAnswer + Ran1;
+                    }
+                    while (1 == 2);
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, that's wrong. Thanks for playing");
+                    break;
+                }
+                Console.ReadLine();
             }
+            Console.ReadLine();
         }
     }
 }
