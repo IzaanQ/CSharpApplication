04c1a029789ebd6fa07ec8f1a796d1ae79b33af6
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2022-03-25 09:14:31 -0400

    Update app.cs

--- app.cs
+++ app.cs
@@ -11,8 +11,10 @@ namespace ConsoleApplication1
         {
             Random rnd = new Random();
             int Ran = rnd.Next(1, 100);
+            //generate random number (#1) with a range from 1 - 100
             Random rnd1 = new Random();
             int Ran1 = rnd1.Next(1, 100);
+            //generate random number (#2) with a range from 1 - 100
             int score = 0; 
             Console.WriteLine("Let's test your math skills!");
             Console.WriteLine("You are required to add a random integer to a random number and see how far you get!");
